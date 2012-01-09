abbbb89ee4094133e90baff97ff5c142ce87cbf7
Andersen Fan <as181920@hotmail.com>
2012-01-09 23:19:53 +0800

    rails new and init git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Asnote::Application.config.secret_token = '5fb176902a6388bda2620b182240ad62d31aab13ba832363963343d6440294f1c8a060e00ad9dacad07ae4593b991d717453fc0ca7d615ea93fe29a72200b800'
