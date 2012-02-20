82e01ecbefc7f5704bfc394ce965c36c9ada137a
andersen_fan <as181920@hotmail.com>
2012-02-21 01:24:02 +0800

    add test case code

--- test/functional/labels_controller_test.rb
+++ test/functional/labels_controller_test.rb
@@ -27,8 +27,8 @@ class LabelsControllerTest < ActionController::TestCase
   test "should get edit" do
     get :edit, note_id: @note_id, id: @label_id
     assert_response :success
-    #TODO: assert_not_nil assigns(:label)
-    #TODO: some more assigns?
+    assert_not_nil assigns(:note)
+    assert_not_nil assigns(:label)
   end
 
   test "should get show of label info" do
@@ -39,14 +39,19 @@ class LabelsControllerTest < ActionController::TestCase
   end
 
   test "create new label and update and delete" do
-    label_params = {label: {name: "label1", comment: "label1 of note1 for controller test"}, note_id: @note_id}
+    label_params = {label: {name: "label2", comment: "label2 of note1 for controller test"}, note_id: @note_id}
     assert_difference('Note.find_one({_id: BSON::ObjectId(@note_id)})["labels"].count', 1) do
       post :create, label_params
     end
     assert_redirected_to note_labels_path(@note_id)
     assert_equal "label successfully created!", flash[:notice]
 
-    #TODO: update test
+    label_new = {label: {name: "label9", comment: "label9 of note1 for controller test"}, note_id: @note_id, id: @label_id}
+    put :update, label_new
+    #TODO: label_new_db = Note.findxxx
+    #TODO: assert_equal label_new[:label][:name], label_new_db[:label][:name]
+    #TODO: assert_equal label_new[:label][:comment], label_new_db[:label][:comment]
+    assert_equal "label successfully updated!", flash[:notice]
 
     assert_difference('Note.find_one({_id: BSON::ObjectId(@note_id)})["labels"].count', -1) do
       delete :destroy, note_id: @note_id, id: @label_id
@@ -56,7 +61,7 @@ class LabelsControllerTest < ActionController::TestCase
   end
 
   test "create new label failed for invalid data" do
-    label_params = {label: {name: "", comment: "label1 of note1 for controller test"}, note_id: @note_id}
+    label_params = {label: {name: "", comment: "label3 of note1 for controller test"}, note_id: @note_id}
     assert_no_difference('Note.find_one({_id: BSON::ObjectId(@note_id)})["labels"].count') do
       post :create, label_params
     end
@@ -65,18 +70,14 @@ class LabelsControllerTest < ActionController::TestCase
     assert_not_nil flash[:error]
   end
 
-  test "update label info" do
+  test "update label info need essential fields" do
+    label_params = {label: {name: "", comment: "label4 of note1 for controller test"}, note_id: @note_id, id: @label_id}
+    put :update, label_params
+    #TODO: label_db = Note.find_one_label(@label_id)
+    #TODO: assert_not_equal label_params[:comment], label_db[:comment]
+    assert_equal "label update failed!", flash[:error]
+    assert_redirected_to note_labels_path(@note_id)
+
   end
 end
 
-
-#######################################################################
-
-=begin
-class NotesControllerTest < ActionController::TestCase
-
-  test "update note info" do
-    assert false, "this feature to be done."
-  end
-end
-=end
